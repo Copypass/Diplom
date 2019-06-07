60dfe7a3a8d831172f7bec624ae3f131a708caf1
Prudov Mihail <miha.prudow@ya.ru>
2019-06-07 05:50:03 +0300

    test5

--- diplom_admin/pages/main.php
+++ diplom_admin/pages/main.php
@@ -16,15 +16,15 @@ if(!isset($_SESSION["id"]))
     <head>
         <title>Forms</title>
         <!-- Bootstrap -->
-        <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
-        <link href="../bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
-        <link href="../assets/styles.css" rel="stylesheet" media="screen">
-        <!--[if lte IE 8]><script language="javascript" type="text/javascript" src="../vendors/flot/excanvas.min.js"></script><![endif]-->
+        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+        <link href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
+        <link href="assets/styles.css" rel="stylesheet" media="screen">
+        <!--[if lte IE 8]><script language="javascript" type="text/javascript" src="vendors/flot/excanvas.min.js"></script><![endif]-->
         <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
         <!--[if lt IE 9]>
             <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
         <![endif]-->
-        <script src="../vendors/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+        <script src="vendors/modernizr-2.6.2-respond-1.1.0.min.js"></script>
     </head>
        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script type="text/javascript">
@@ -125,27 +125,27 @@ if(!isset($_SESSION["id"]))
             </footer>
         </div>
         <!--/.fluid-container-->
-        <link href="../vendors/datepicker.css" rel="stylesheet" media="screen">
-        <link href="../vendors/uniform.default.css" rel="stylesheet" media="screen">
-        <link href="../vendors/chosen.min.css" rel="stylesheet" media="screen">
+        <link href="vendors/datepicker.css" rel="stylesheet" media="screen">
+        <link href="vendors/uniform.default.css" rel="stylesheet" media="screen">
+        <link href="vendors/chosen.min.css" rel="stylesheet" media="screen">
 
-        <link href="../vendors/wysiwyg/bootstrap-wysihtml5.css" rel="stylesheet" media="screen">
+        <link href="vendors/wysiwyg/bootstrap-wysihtml5.css" rel="stylesheet" media="screen">
 
-        <script src="../vendors/jquery-1.9.1.js"></script>
-        <script src="../bootstrap/js/bootstrap.min.js"></script>
-        <script src="../vendors/jquery.uniform.min.js"></script>
-        <script src="../vendors/chosen.jquery.min.js"></script>
-        <script src="../vendors/bootstrap-datepicker.js"></script>
+        <script src="vendors/jquery-1.9.1.js"></script>
+        <script src="bootstrap/js/bootstrap.min.js"></script>
+        <script src="vendors/jquery.uniform.min.js"></script>
+        <script src="vendors/chosen.jquery.min.js"></script>
+        <script src="vendors/bootstrap-datepicker.js"></script>
 
-        <script src="../vendors/wysiwyg/wysihtml5-0.3.0.js"></script>
-        <script src="../vendors/wysiwyg/bootstrap-wysihtml5.js"></script>
+        <script src="vendors/wysiwyg/wysihtml5-0.3.0.js"></script>
+        <script src="vendors/wysiwyg/bootstrap-wysihtml5.js"></script>
 
-        <script src="../vendors/wizard/jquery.bootstrap.wizard.min.js"></script>
+        <script src="vendors/wizard/jquery.bootstrap.wizard.min.js"></script>
 
-	<script type="text/javascript" src="../vendors/jquery-validation/dist/jquery.validate.min.js"></script>
-	<script src="../vendorsassets/form-validation.js"></script>
+	<script type="text/javascript" src="vendors/jquery-validation/dist/jquery.validate.min.js"></script>
+	<script src="vendorsassets/form-validation.js"></script>
         
-	<script src="../vendorsassets/scripts.js"></script>
+	<script src="vendorsassets/scripts.js"></script>
         <script>
 
 	jQuery(document).ready(function() {   
