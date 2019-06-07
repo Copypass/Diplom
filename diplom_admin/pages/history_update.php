72d845dbfa42160daa71fb3f66afce8d2888d50a
Prudov Mihail <miha.prudow@ya.ru>
2019-06-07 05:29:42 +0300

    17

--- diplom_admin/pages/history_update.php
+++ diplom_admin/pages/history_update.php
@@ -12,7 +12,7 @@
 		error_reporting(E_ALL);
 		ini_set('display_errors', TRUE);
 		ini_set('display_startup_errors', TRUE);
-		session_start();
+		//session_start();
 		$key_id = "";
 		$name = "";
 		$img = "";
